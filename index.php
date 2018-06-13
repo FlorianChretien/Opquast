ca5c9f22b88db627d96117fe2150f674b7f53449
Florian Chrétien <f.chretien@oenodata.fr>
2018-06-13 09:56:38 +0200

    second

---
+++ index.php
@@ -0,0 +1,198 @@
+<?php get_header() ?>
+
+    <section class="wrap-overview">
+        <div class="container">
+            <div class="title-overview">
+                <h1>Lorem ipsum dolor sit amet</h1>
+                <p class="baseline-overview">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+
+            <div class="wrap-button">
+                <a href="#" class="btn-primary">Découvrir</a>
+                <a href="#" class="btn-primary">Test gratuit</a>
+            </div>
+
+            <div>
+                <div>
+                    <img src="https://picsum.photos/70/70" alt="">
+                    <h4>Certification</h4>
+                    <button>Decouvrir</button>
+                </div>
+                <div>
+                    <img src="https://picsum.photos/90/90" alt="">
+                    <h4>Formation</h4>
+                    <button>Decouvrir</button>
+                </div>
+                <div>
+                    <img src="https://picsum.photos/90/90" alt="">
+                    <h4>Cheklists</h4>
+                    <button>Decouvrir</button>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section class="">
+        <div>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h3>Cras hendrerit tortor erat, non suscipit nunc pharetra sed.</h3>
+        </div>
+        <div>
+            <h3>Iphone App</h3>
+            <h4>Cras hendrerit tortor erat, non suscipit nunc.</h4>
+            <div>
+                <img src="https://picsum.photos/50/50" alt="">
+                <h4>Feature #1</h4>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <div>
+                <img src="https://picsum.photos/50/50" alt="">
+                <h4>Feature #2</h4>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <div>
+                <img src="https://picsum.photos/50/50" alt="">
+                <h4>Feature #3</h4>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <button>Contact</button>
+        </div>
+    </section>
+    <section class="">
+        <div>
+            <img src="https://picsum.photos/720/855" alt="">
+        </div>
+        <div>
+            <div>
+                <h2>Certification</h2>
+                <h4>Lorem ipsum dolor sit alem, consecteur adipising emilie.</h4>
+            </div>
+            <div>
+                <img src="https://picsum.photos/50/50" alt="">
+                <h3>Feature #1</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <div>
+                <img src="https://picsum.photos/50/50" alt="">
+                <h3>Feature #2</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <div>
+                <img src="https://picsum.photos/50/50" alt="">
+                <h3>Feature #3</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+        </div>
+    </section>
+    <section class="">
+        <div>
+            <div>
+                <h2>Formations</h2>
+                <h3>Lorem ipsum dolor sit alem, consecteur adipising emilie.</h3>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h3>Feature #1</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h3>Feature #2</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h3>Feature #3</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+        </div>
+        <div>
+            <img src="https://picsum.photos/720/855" alt="">
+        </div>
+    </section>
+    <section class="">
+        <div>
+            <img src="https://picsum.photos/720/855" alt="">
+        </div>
+        <div>
+            <div>
+                <h2>Checklists</h2>
+                <h3>Lorem ipsum dolor sit alem, consecteur adipising emilie.</h3>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h3>Feature #1</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h3>Feature #2</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <h3>Feature #3</h3>
+                <p>Morbi metus dolor, maximus id massa sit amet, cursus semper nulla.</p>
+            </div>
+        </div>
+    </section>
+    <section class="">
+        <div>
+            <button><</button>
+            <div>
+                <div>
+                    <img src="https://picsum.photos/280/280" alt="">
+                    <h3>Emilie Rozis</h3>
+                    <h4>Ux design</h4>
+                </div>
+                <div>
+                    <img src="https://picsum.photos/280/280" alt="">
+                    <h3>John Doe</h3>
+                    <h4>Developpeur</h4>
+                </div>
+                <div>
+                    <img src="https://picsum.photos/280/280" alt="">
+                    <h3>Florian Chrétien</h3>
+                    <h4>Webmarketing</h4>
+                </div>
+            </div>
+            <button>></button>
+        </div>
+    </section>
+    <section class="">
+        <h2>Ils nous font confiance</h2>
+        <div>
+            <button><</button>
+            <div>
+                <div class="header">
+                    <img src="https://picsum.photos/255/37" alt="">
+                    <img src="https://picsum.photos/158/37" alt="">
+                    <img src="https://picsum.photos/160/37" alt="">
+                    <img src="https://picsum.photos/260/37" alt="">
+                </div>
+                <div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et elementum metus. Suspendisse libero libero, hendrerit vel sollicitudin et, posuere ac massa.
+                        Aliquam enim mi, ullamcorper ut ipsum sed, aliquam vestibulum quam. Aliquam vel metus auctor, sollicitudin purus ac, varius ipsum.
+                    </p>
+                </div>
+            </div>
+            <button>></button>
+        </div>
+    </section>
+    <section class="">
+        <h2>Testimonials</h2>
+        <button></button>
+        <div>
+            <img src="https://picsum.photos/334/334" alt="">
+            <div>
+                <img src="https://picsum.photos/40/32" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et elementum metus. Suspendisse libero libero, hendrerit vel. Bla varius ipsum da sed elit.</p>
+                <h6>John Doe</h6>
+                <p>CEO of the best compagny</p>
+            </div>
+        </div>
+        <button></button>
+    </section>
+
+<?php get_footer() ?>
