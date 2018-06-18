9d34a9d5a1626f5fd43286dcb16eae034fcd6a42
Emilie ROZIS <emilie.40@hotmail.com>
2018-06-18 09:08:12 +0200

    css for home page

--- header-opquast.php
+++ header-opquast.php
@@ -10,30 +10,35 @@
 
     </head>
 <body>
+    <div class="header">
+        <video autoplay loop id="bg-video">
+            <source src="" type="">
+        </video>
 
+        <header class="wrap-header">
+            <div class="container">
+                <a href="<?php echo get_site_url() ?>" class="link-home">
+                    <img src="<?php echo get_site_url() ?>/wp-content/uploads/2018/06/logo_opquast_no-baseline.png" alt="Logotype Opquast" class="img-logo">
+                </a>
 
-<header class="wrap-header">
-    <div class="container">
-        <div id="menu-btn">
-            <span class="icon"></span>
-            <span class="text">MENU</span>
-        </div>
+                <div class="wrap-links">
+                    <nav role="navigation" class="wrap-navigation">
+                    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+                    </nav>
 
-        <a href="<?php echo get_site_url() ?>" class="link-home">
-            <img src="<?php echo get_site_url() ?>/wp-content/uploads/2018/06/logo_opquast_no-baseline.png" alt="Logotype Opquast" class="img-logo">
-        </a>
+                    <a href="<?php echo get_site_url() ?>/login/" class="link-login btn btn-primary">Connexion</a>
 
-        <nav role="navigation" class="wrap-navigation">
-        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-        </nav>
+                    <div id="menu-btn">
+                        <span class="icon"></span>
+                        <span class="text">MENU</span>
+                    </div>
+                </div>
 
-        <a href="<?php echo get_site_url() ?>/login/" class="link-login btn btn-primary">Connexion</a>
-
-<!--        <form role="search" method="get" id="searchform" class="searchform"-->
-<!--              action="--><?php //echo get_site_url() ?><!--">-->
-<!--            <!--<label class="screen-reader-text" for="s">Search for:</label>-->
-<!--            <input type="search" value="" name="s" id="s" placeholder="Recherche"/>-->
-<!--            <!--<input type="submit" id="searchsubmit" value="Search" />-->
-<!--        </form>-->
-    </div>
-</header>
+        <!--        <form role="search" method="get" id="searchform" class="searchform"-->
+        <!--              action="--><?php //echo get_site_url() ?><!--">-->
+        <!--            <!--<label class="screen-reader-text" for="s">Search for:</label>-->
+        <!--            <input type="search" value="" name="s" id="s" placeholder="Recherche"/>-->
+        <!--            <!--<input type="submit" id="searchsubmit" value="Search" />-->
+        <!--        </form>-->
+            </div>
+        </header>
